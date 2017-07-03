fe8e03fd1bbe3d62af1febf63bf77f371d6e868d
marshalldanel <marshalldanel@gmail.com>
2017-07-03 19:42:49 +0000

    Client uppercase module

---
+++ day-1/https-client/callbacks/printUppercase.js
@@ -0,0 +1,12 @@
+var getHTML = require('../httpFunctions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUppercase(html) {
+  console.log(html.toUpperCase());
+}
+
+getHTML(requestOptions, printUppercase);
